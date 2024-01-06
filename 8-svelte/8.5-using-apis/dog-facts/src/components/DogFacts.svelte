d6dbfb5cc20cb283991b1356bb1fa87acc9358e6
MM <michele.makiki@gmail.com>
2024-01-06 16:54:51 +0200

    8.5 Using APIs

---
+++ 8-svelte/8.5-using-apis/dog-facts/src/components/DogFacts.svelte
@@ -0,0 +1,21 @@
+<script>
+	const fetchFact = async () => {
+		const response = await fetch(' https://dogapi.dog/api/v2/facts');
+    return await response.json();
+	};
+
+	let factPromise = '';
+	const updateFact = () => {
+		fetchFact()
+      .then((response) => factPromise = response.data[0].attributes.body)
+	};
+
+</script>
+
+<button on:click={updateFact}> Fetch a dog fact</button>
+
+{#await factPromise}
+  <p>Loading...</p>
+{:then fact}
+<p>{fact}</p>
+{/await}
---
+++ 8-svelte/8.5-using-apis/dog-facts/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import JokeApi from "../components/JokeApi.svelte";
+  import DogFacts from "../components/DogFacts.svelte";
+</script>
+
+<DogFacts />
