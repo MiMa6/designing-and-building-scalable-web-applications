b2a1be53cea2d168d0c3b4b4f15122054ba37fde
MM <michele.makiki@gmail.com>
2024-01-06 18:09:29 +0200

    9.1 Introduction to Astro and tooling

---
+++ 9-astro/hello-astro-and-svelte/src/components/Hello.svelte
@@ -0,0 +1,10 @@
+<script>
+  let message = "Svelte";
+
+  const updateText = () => {
+    message === "Svelte" ? (message = "Astro") : (message = "Svelte");
+  };
+</script>
+
+<h1>Hello {message}!</h1>
+<button on:click={updateText}>Update!</button>
