1ad194ab25350e482149b10614e4fd83e4e57b4b
MM <michele.makiki@gmail.com>
2024-01-09 13:31:34 +0200

    10.3 Styling components

---
+++ 10-tailwind/10.3-styling-components/simple-card/src/components/Card.svelte
@@ -0,0 +1,13 @@
+<div class="max-w-72 max-h-48 rounded overflow-hidden shadow-lg">
+  <div class="px-6 py-4">
+    <h2 class="font-bold text-xl mb-2">Card</h2>
+    <p class="text-gray-700 text-base">
+      Description
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#hashtag1</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#hashtag2</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#hashtag3</span>
+  </div>
+</div>
