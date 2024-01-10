a496a7c72c78cf9c96cc9f73b27b0ace1e0fa350
MM <michele.makiki@gmail.com>
2024-01-10 10:49:00 +0200

    10.4 & 10.5 - Common layouts and Responsive Design

--- 10-tailwind/simple-card/src/components/Card.svelte
+++ 10-tailwind/simple-card/src/components/Card.svelte
@@ -8,6 +8,5 @@
   <div class="px-6 pt-4 pb-2">
     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#hashtag1</span>
     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#hashtag2</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#hashtag3</span>
   </div>
 </div>
---
+++ 10-tailwind/simple-card/src/components/CardCollection.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Card from "./Card.svelte";
+</script>
+
+<section class="container mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-2">
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+</section>
